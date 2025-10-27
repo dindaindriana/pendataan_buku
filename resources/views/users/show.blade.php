f54f7ddc44841fb51b492601502b61d727e1fbb2
dindaindriana <dindaindriana0911@gmail.com>
2025-10-27 11:45:58 +0700

    delete user

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -11,7 +11,39 @@
         <div class="card-body">
             <h5 class="card-title">{{ $user->name }}</h5>
             <p class="card-text">{{ $user->email }}</p>
-            <a href="#" class="btn btn-primary">Edit</a>
+            <button type="button" class="btn btn-danger " data-bs-toggle="modal" data-bs-target="#deleteModal{{ $user->id }}">
+                Delete
+            </button>
         </div>
+
+
+
+        {{-- Modal delete --}}
+        <div class="modal fade" id="deleteModal{{ $user->id }}" tabindex="-1"
+            aria-labelledby="deleteModalLabel{{ $user->id }}" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Hapus User</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p> Apakah kamu yakin ingin menghapus data <strong>{{ $user->name }}</strong>?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                        <!-- Form Delete -->
+                        <form action="/users/{{ $user->id }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Hapus</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{-- End modal deelte --}}
+
     </div>
 </x-app-layout>
